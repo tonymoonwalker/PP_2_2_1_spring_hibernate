d5f8f8a953e7d784727d874216a30881da332683
tonymoonwalker <tonymoonwalker@users.noreply.github.com>
2023-09-30 10:49:55 +0300

    Updated query code

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -28,8 +28,10 @@ public class UserDaoImp implements UserDao {
 
    @Override
    public List<User> getUserByCar(String model, int series) {
-      String hqlQuery = "FROM User u WHERE u.car.id = (SELECT c.id FROM Car c WHERE c.model = :model AND c.series = :series)";
+      String hqlQuery = "FROM User u WHERE u.car.id IN (SELECT c.id FROM Car c WHERE c.model = :model AND c.series = :series)";
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery(hqlQuery);
+      query.setParameter("model", model);
+      query.setParameter("series", series);
       return query.getResultList();
    }
 }
